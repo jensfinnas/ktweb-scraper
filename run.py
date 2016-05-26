d31e8b22bfc8f400377dc2480905424e5bc7052d
rotsee <leo_wallentin@hotmail.com>
2016-05-26 17:40:57 +0200

    create s3 key

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from urllib2 import urlopen, HTTPError
+from urllib2 import urlopen, HTTPError, quote
 from time import sleep
 
 try:
@@ -14,15 +14,25 @@ from modules.site import Site
 
 site = Site(settings.ktweb_url)
 
+
+def build_path(*args):
+    """ Build a urlencoded slash separated string
+    """
+    list_ = [quote(x.encode("utf-8")) for x in args]
+    return '/'.join(list_)
+
 for body in site.bodies():
     for meeting in site.meetings(body.name):
         for document in meeting.documents():
             sleep(settings.delay)
-            print document.url
             try:
                 response = urlopen(document.url)
             except HTTPError:
                 print "failed to open %s" % document.url
                 continue
+
+            """ Include size in key, just in case """
             size = response.info()["Content-Length"]
-            print size
+            date_str = meeting.date.strftime("%Y-%m-%d")
+            key = build_path(body.name, date_str, document.name, size)
+            print key
