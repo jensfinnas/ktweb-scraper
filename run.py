82ee5f1b9a967d28a956c5b7de8e75bafa93616c
rotsee <leo_wallentin@hotmail.com>
2016-05-26 16:22:57 +0200

    test

--- run.py
+++ run.py
@@ -1,10 +1,28 @@
 # encoding: utf-8
 
+from urllib2 import urlopen, HTTPError
+from time import sleep
+
 try:
-    import settingse
+    import settings
 except ImportError:
     print """Create a settings.py file to get started:
 cp settings.default.py settings.py"""
     exit()
 
-print settings.ktweb_url
+from modules.site import Site
+
+site = Site(settings.ktweb_url)
+
+for body in site.bodies():
+    for meeting in site.meetings(body.name):
+        for document in meeting.documents():
+            sleep(settings.delay)
+            print document.url
+            try:
+                response = urlopen(document.url)
+            except HTTPError:
+                print "failed to open %s" % document.url
+                continue
+            size = response.info()["Content-Length"]
+            print size
