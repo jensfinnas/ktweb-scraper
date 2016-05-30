0432c904130e1519d45abbcb62dca97fe44ed33f
rotsee <leo_wallentin@hotmail.com>
2016-05-30 21:40:22 +0200

    use UI; identify when visiting pages

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from urllib2 import urlopen, HTTPError, quote
+from urllib2 import urlopen, HTTPError, quote, Request
 from time import sleep
 from modules.s3 import Bucket, open_s3_file
 # Make sure settings.py is set up
@@ -13,6 +13,7 @@ cp settings.default.py settings.py"""
     exit()
 from modules.site import Site
 from modules.filetype import FileType
+from modules.interface import Interface
 
 
 def build_path(*args):
@@ -22,6 +23,12 @@ def build_path(*args):
     return '/'.join(list_)
 
 
+cmd_args = [
+]
+ui = Interface("Run",
+               "Scrape, extract and store documents",
+               commandline_args=cmd_args)
+
 site = Site(settings.ktweb_url)
 for body in site.bodies():
     for meeting in site.meetings(body.name):
@@ -29,9 +36,10 @@ for body in site.bodies():
 
             sleep(settings.delay)
 
-            # Get headers
             try:
-                response = urlopen(document.url)
+                req = Request(document.url)
+                req.add_header('User-agent', settings.user_agent)
+                response = urlopen(req)
             except HTTPError:
                 print "failed to contact %s" % document.url
                 continue
@@ -40,7 +48,10 @@ for body in site.bodies():
             size = info["Content-Length"]
             content_type = info["Content-Type"]
             # Get file name from `application/octet-stream; name=64248323.doc`
-            file_name = content_type.split("; ")[1].split("=")[1]
+            try:
+                file_name = content_type.split("; ")[1].split("=")[1]
+            except IndexError:
+                file_name = ""
             date_str = meeting.date.strftime("%Y-%m-%d")
             key = build_path(body.name, date_str,
                              document.name + "-" + file_name)
@@ -49,22 +60,24 @@ for body in site.bodies():
             # TODO
 
             # Put file on S3
+            ui.info("Putting %s on Amazon S3" % document.url)
             bucket = Bucket(settings.s3_bucket)
+            if ui.args["dryrun"]:
+                continue
             bucket.put_file_from_url(document.url, "files/" + key)
 
-            # Extract mimetype, text and metadata
+            # Do extraction
             with open_s3_file(bucket, "files/" + key) as file_:
                 filetype = FileType()
                 mimetype = filetype.get_mime_type(file_)
                 ext = filetype.type_to_ext[mimetype]
                 Extractor = filetype.type_to_extractor[mimetype]
+                extractor = Extractor(file_.name)
+                text = extractor.get_text()
                 print file_.name
                 print ext
                 print Extractor
-                extractor = Extractor(file_.name)
-                text = extractor.get_text()
                 print text
-                # print process(file_.name, parser="doc")
 
             exit()
             # Add text to S3
--- settings.default.py
+++ settings.default.py
@@ -4,3 +4,12 @@ Use settings.py override any of these settings.
 """
 
 ktweb_url = "http://ktweb.tampere.fi/ktwebbin/dbisa.dll/ktwebscr/"
+
+s3_bucket = "ktweb.tampere"
+""" Amazon S3 bucket where files should be stored """
+
+delay = 1
+""" Number of seconds to sleep between downloads """
+
+user_agent = "Mozilla/5.0 (compatible); Alma document scraper"
+"""How do we identify when visiting web pages?"""
