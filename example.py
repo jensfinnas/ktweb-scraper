f85b92fb9d070814a163e31ed0103d0327c7f1fc
jensfinnas <jens.finnas@gmail.com>
2016-07-01 21:50:49 +0200

    Update example

--- example.py
+++ example.py
@@ -5,9 +5,8 @@ site = Site("http://ktweb.tampere.fi/ktwebbin/dbisa.dll/ktwebscr/")
 
 from datetime import datetime
 
-for meeting in site.meetings("Kaupunginhallitus", after_date="2016-01-01"):
+for meeting in site.meetings("Kaupunginhallitus", after_date="2016-07-01"):
     print meeting
     for doc in meeting.minutes():
         print doc
-        print "Parent: " + str(doc.parent_paragraph)
         #doc.download()
