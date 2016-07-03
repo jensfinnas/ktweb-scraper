b8512d58f063acd60c0974513c7f2e74075e26e7
rotsee <leo_wallentin@hotmail.com>
2016-07-03 22:28:43 +0200

    -unused code

--- example.py
+++ example.py
@@ -3,10 +3,7 @@ from modules.site import Site
 
 site = Site("http://ktweb.tampere.fi/ktwebbin/dbisa.dll/ktwebscr/")
 
-from datetime import datetime
-
 for meeting in site.meetings("Kaupunginhallitus", after_date="2016-07-01"):
     print meeting
     for doc in meeting.minutes():
         print doc
-        #doc.download()
